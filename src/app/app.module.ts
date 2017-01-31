2b600300fee835f2385cf20bd3cc666583cbae1c
Ivan Stanojevic <ivan@byteout.com>
2017-01-31 14:01:49 +0100

    a2 init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MomentModule } from 'angular2-moment';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAFmRtARCG2OZMVtbj8GixI2XM9lOiZIow'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
